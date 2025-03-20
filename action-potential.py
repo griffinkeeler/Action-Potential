ae03e8c69cefd177c455bb0efa621a4a13a6a6e5
Griffin Keeler <144262024+griffinkeeler@users.noreply.github.com>
2025-03-20 13:51:31 -0500

    Add files via upload

---
+++ action-potential.py
@@ -0,0 +1,17 @@
+# mV of membrane at different stages of
+# the Action Potential
+millivolts = 0
+resting_potential = -65
+threshold = -55
+peak = 30
+
+# Boolean values of na and k
+# channels at resting potential.
+na_channels = bool
+k_channels = bool
+
+# Charge of ions
+na = 1
+k = 1
+
+# PHASE 1: Resting Potential
